e2bdca9f3c569bc67104ca36c609c365aee4531e
AngelRoEst <jiucastellpandor@gmail.com>
2024-03-13 22:31:53 -0600

    Punto 3 - Jugos

--- jugos.js
+++ jugos.js
@@ -4,7 +4,7 @@ Eres un cliente frecuente de su tienda y te diste cuenta de que podías hacerle 
 fácil a tu amiga. Decides utilizar tus habilidades de codificación para ayudar a Li Mei con
 su trabajo.
 */
-//1_ Funcion para el tiempo de preparacion---------------------------------------------------------------------
+//Funcion para el tiempo de preparacion
 function timeToMixJuice(jugo) {
     switch(jugo) { //gracias profe :)
         case 'Pure Strawberry Joy':
@@ -61,3 +61,46 @@ function limesToCut(cantidadNecesaria, limasDisponibles) {
 
     return limasParaCortar;
 }
+
+
+
+/*
+Li Mei siempre trabaja hasta las 3 de la tarde. Luego su empleado Dmitry se hace cargo. A
+menudo hay bebidas que se han pedido pero que aún no están preparadas cuando
+termina el turno de Li Mei. Luego, Dmitry preparará los jugos restantes.
+Para facilitar la entrega, implemente una función de pedidos restantes que toma la
+cantidad de minutos que quedan en el turno de Li Mei y una variedad de jugos que se han
+pedido pero que aún no se han preparado. La función debería devolver los pedidos que Li
+Mei no puede empezar a preparar antes del final de su jornada laboral.
+El tiempo restante en el turno siempre será mayor a 0. El arsenal de jugos a preparar nunca
+estará vacío. Además, los pedidos se preparan en el orden en que aparecen en la matriz. Si
+Li Mei comienza a mezclar cierto jugo, siempre lo terminará incluso si tiene que trabajar un
+poco más. Si no quedan órdenes restantes de las que Dmitry deba encargarse, se debe
+devolver una matriz vacía.*/
+
+
+function jugosEnEspera(tiempoRestante, jugosRestantes)
+{
+    if (tiempoRestante <= 0 )
+    {
+        return "No se puede ejecutar esta función por un tiempo restante incongruente";
+    }
+    else if (jugosRestantes.length === 0)
+    {
+        return "No se puede usar esta función por un parametro de jugos restantes incongruente";
+    }
+    else
+    {
+        while(tiempoRestante > 0)
+        {
+            tiempoRestante = tiempoRestante - timeToMixJuice(jugosRestantes[0]);
+            tiempoRestante.shift();
+        }
+        return jugosRestantes;
+    }
+
+    
+
+
+
+} 
